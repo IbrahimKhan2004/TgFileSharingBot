97ec01763aa96fe2a1b3fb076088a499a3969fcb
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2025-10-09 12:29:29 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 import pymongo
 from config import MONGO_URI, MONGO_DB_NAME
+from time import time as tm
 
 dbclient = pymongo.MongoClient(MONGO_URI)
 database = dbclient[MONGO_DB_NAME]
 
 user_data = database['users']
+banned_users = database['banned_users']
 
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
@@ -25,3 +27,26 @@ async def full_userbase():
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
+
+async def ban_user(user_id: int, ban_duration: int):
+    """Bans a user for a specified duration (in seconds)."""
+    ban_until = tm() + ban_duration
+    banned_users.update_one(
+        {'_id': user_id},
+        {'$set': {'ban_until': ban_until}},
+        upsert=True
+    )
+    return
+
+async def is_user_banned(user_id: int):
+    """Checks if a user is currently banned."""
+    user = banned_users.find_one({'_id': user_id})
+    if user:
+        ban_until = user.get('ban_until', 0)
+        if tm() < ban_until:
+            return True
+        else:
+            # Ban expired, remove from banned_users collection
+            banned_users.delete_one({'_id': user_id})
+            return False
+    return False
